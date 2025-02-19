ed5285c61b2d58d25829475d62a2ac7f7ec2542b
Jacob Sorrells <Sorreljh@dukes.jmu.edu>
2025-02-18 19:56:18 -0500

    Organized Django Admin detail views with fieldsets

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,6 +3,7 @@ from .models import Author, Genre, Book, BookInstance
 #admin.site.register(Book)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 admin.site.register(Author, AuthorAdmin)
 
@@ -15,6 +16,15 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 #admin.site.register(BookInstance)
 # admin.site.register(Language)
